d2ffab2aa2c09e8ce50313eac24ac3f146ffb6fe
jaydeep-kuber <jayofficial085@gmail.coom>
2024-07-31 00:27:22 +0530

    final web app

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -48,6 +49,14 @@ def evalute_model(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as f:
+            return dill.load(f)
 
     except Exception as e:
         raise CustomException(e, sys)
